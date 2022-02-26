75835bd3684f0c1a444dd240576256bcaf00261f
pedroleandro <pedro.leandrog@gmail.com>
2022-02-26 12:51:59 -0300

    Criação das seeders para usuários e produtos.

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Product;
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::create([
+            "name" => "ITALIA INDEPENDENT 90",
+            "description" => "Óculos de Sol Italia Independent 0090 - Cinza Estampado Aveludado - Espelhado Cinza - 000/49",
+            "brand" => "Italia Independent",
+            "category" => "Óculos de Sol",
+            "price" => "220.50",
+            "color" => "Cinza"
+        ]);
+
+        Product::create([
+            "name" => "ITALIA INDEPENDENT 0092C",
+            "description" => "Óculos de Sol Italia Independent 0092C - Preto - 009/60",
+            "brand" => "Italia Independent",
+            "category" => "Óculos de Sol",
+            "price" => "330.60",
+            "color" => "Preto"
+        ]);
+
+        Product::create([
+            "name" => "ITALIA INDEPENDENT 0900T",
+            "description" => "Óculos de Sol Italia Independent 0900T - Preto Estampado - T/50",
+            "brand" => "Italia Independent",
+            "category" => "Óculos de Sol",
+            "price" => "238.5",
+            "color" => "Preto"
+        ]);
+
+        Product::create([
+            "name" => "VOGUE VO5309L",
+            "description" => "Óculos de Grau Vogue VO5309L - Vinho Transparente - 2798/54",
+            "brand" => "Vogue",
+            "category" => "Óculos de Grau",
+            "price" => "162",
+            "color" => "Vinho"
+        ]);
+    }
+}
