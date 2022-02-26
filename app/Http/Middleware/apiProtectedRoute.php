f2354fb23188c91dfed9fd9e87d1e2097f67bf94
pedroleandro <pedro.leandrog@gmail.com>
2022-02-25 23:34:33 -0300

    Configuração e implementação JWT.

--- app/Http/Middleware/apiProtectedRoute.php
+++ app/Http/Middleware/apiProtectedRoute.php
@@ -3,18 +3,34 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Exception;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
 
-class apiProtectedRoute
+class apiProtectedRoute extends BaseMiddleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (Exception $exception) {
+
+            if ($exception instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
+                return response()->json(['status' => 'Token é inválido']);
+            } else if ($exception instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+                return response()->json(['status' => 'Token está expirado']);
+            } else {
+                return response()->json(['status' => 'Token de autorização não encontrado']);
+            }
+        }
+
         return $next($request);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(["namespace" => "Api", "as" => "api."], function () {
 
-    Route::apiResource("/product", "ProductController");
+    Route::post('login', 'AuthController@login')->name('login');
+
+    Route::group(['middleware' => ['apiJwt']], function(){
+
+        Route::post('logout', 'AuthController@logout')->name('logout');
+        Route::apiResource("/product", "ProductController");
+
+    });
 
 });
